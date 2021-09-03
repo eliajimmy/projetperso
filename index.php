d169148bc60df7979ee85e5cc8d2ddbc903314c7
JIMMY ELIA <eliajimmy1@gmail.com>
2021-09-03 09:21:58 +0100

    CREATE INDEX.PHP

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'accueil</title>
+</head>
+<body>
+    <h1>Bienvenue a la page d'accueil</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ducimus, fuga alias beatae quae veritatis totam quam repellat facilis praesentium accusamus quisquam voluptatibus veniam iste, omnis pariatur nihil eligendi voluptatum!</p>
+    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ducimus, fuga alias beatae quae veritatis totam quam repellat facilis praesentium accusamus quisquam voluptatibus veniam iste, omnis pariatur nihil eligendi voluptatum!</p>
+</body>
+</html>
